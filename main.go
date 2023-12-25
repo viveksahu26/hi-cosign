3748734dbd02b3466198e3851a78efca8b7f7742
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2023-12-25 20:58:15 +0530

    remove path from print
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hey Welcome to my page !!, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hey Welcome to my page !!")
 	})
 	// Hello!
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
