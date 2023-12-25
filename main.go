f98d4e889679ea0ca5b6ffcf75ae3fe36caee262
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2023-12-25 21:09:57 +0530

    update main program
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hey Welcome to my page !!")
 	})
-	// Hello!
+	// Hi Cosign !!
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi Cosign !!")
 	})
 
 	log.Fatal(http.ListenAndServe(":3035", nil))
+	fmt.Println("app running on port: ", 3035)
 }
